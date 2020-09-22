35364580161f8b84cbaff509c165a372e804cd7f
Safiyah Lakhany <salakhany@gmail.com>
2020-09-21 23:40:20 -0700

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Safiyah Lakhany on 9/21/20.
+//  Copyright © 2020 Safiyah Lakhany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
