586fa9b66b2ec4fe6b91ff2403a11f7798b86dcb
Safiyah Lakhany <salakhany@gmail.com>
2020-10-02 21:47:16 -0700

    Part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Safiyah Lakhany on 10/2/20.
+//  Copyright © 2020 Safiyah Lakhany. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
